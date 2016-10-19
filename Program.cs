73a958b70484a852b868d2b82624a7eb0ba542cd
Michael Hauss <mihauss@microsoft.com>
2016-10-19 15:45:47 -0700

    Code reviewed

---
+++ Program.cs
@@ -0,0 +1,162 @@
+﻿//----------------------------------------------------------------------------------
+// Microsoft Developer & Platform Evangelism
+//
+// Copyright (c) Microsoft Corporation. All rights reserved.
+//
+// THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, 
+// EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES 
+// OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+//----------------------------------------------------------------------------------
+// The example companies, organizations, products, domain names,
+// e-mail addresses, logos, people, places, and events depicted
+// herein are fictitious.  No association with any real company,
+// organization, product, domain name, email address, logo, person,
+// places, or events is intended or should be inferred.
+//----------------------------------------------------------------------------------
+
+namespace Incremental_Snapshot_Backup
+{
+    using Microsoft.WindowsAzure;
+    using Microsoft.WindowsAzure.Storage;
+    using Microsoft.WindowsAzure.Storage.Blob;
+    using Microsoft.WindowsAzure.Storage.RetryPolicies;
+    using Microsoft.WindowsAzure.Storage.DataMovement;
+
+    using System;
+    using System.Threading.Tasks;
+    using System.IO;
+    using System.Collections.Generic;
+
+    public class Program
+    {
+        // References to the containers that will be used in this simulation
+        static CloudBlobContainer container;
+        static CloudBlobContainer backupContainer;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Incremental Snapshot Backup Sample\n");
+            try
+            {
+                IncrementalSnapshotBackupAsync().Wait();
+            }
+            finally
+            {
+                // Clean up after the demo. To inspect the results of this sample in the Azure Portal,
+                // comment out the following two lines of code.
+                container.DeleteIfExists();
+                backupContainer.DeleteIfExists();
+            }
+
+            Console.WriteLine("Completed succesfully\nPress the enter key to exit");
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// Demonstrates how to do backup using incremental snapshots. See this article on 
+        /// backing up Azure virtual machine disks with incremental snapshots:
+        /// https://azure.microsoft.com/en-us/documentation/articles/storage-incremental-snapshots/
+        /// The article also describes how to restore a disk from incremental snapshots.
+        /// </summary>
+        /// <returns>Task</returns>
+        public static async Task IncrementalSnapshotBackupAsync()
+        {
+            const int TotalBlobSize = 512 * 6;      // The total amount of data in the blob
+            const int UpdateWriteSize = 512 * 2;    // The amount of data to update in the blob
+            const int UpdateWriteOffset = 512 * 2;  // The offset at which to write updated data
+            const int ClearPagesOffset = 512 * 5;   // The offset at which to begin clearing page data
+            const int ClearPagesSize = 512;         // The amount of data to clear in the blob
+
+            string SimulationID = Guid.NewGuid().ToString();      // The simulation ID
+            string ContainerName = "container-" + SimulationID;   // The name of the primary container
+            string PageBlobName = "binary-data";                  // The name of the primary page blob
+
+            // Retrieve storage account information from connection strings
+            // How to create storage connection strings - http://msdn.microsoft.com/en-us/library/azure/ee758697.aspx
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(
+               CloudConfigurationManager.GetSetting("StorageConnectionString"));
+            CloudStorageAccount backupStorageAccount = CloudStorageAccount.Parse(
+               CloudConfigurationManager.GetSetting("BackupStorageConnectionString"));
+
+            // Create blob clients for interacting with the blob service.
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+            CloudBlobClient backupBlobClient = backupStorageAccount.CreateCloudBlobClient();
+
+            // Create containers for organizing blobs within the storage accounts.
+            container = blobClient.GetContainerReference(ContainerName);
+            backupContainer = backupBlobClient.GetContainerReference("copy-of-" + ContainerName);
+            try
+            {
+                // The call below will fail if the sample is configured to use the storage emulator 
+                // in the connection string, but the emulator is not running.
+                BlobRequestOptions requestOptions = new BlobRequestOptions()
+                { RetryPolicy = new NoRetry() };
+                await container.CreateIfNotExistsAsync(requestOptions, null);
+                await backupContainer.CreateIfNotExistsAsync(requestOptions, null);
+            }
+            catch (StorageException)
+            {
+                Console.WriteLine("If you are running with the default connection string, please make " +
+                    "sure you have started the storage emulator. Press the Windows key and type Azure " +
+                    "Storage to select and run it from the list of applications - then restart the sample.");
+                Console.ReadLine();
+                throw;
+            }
+
+            // Create, write to, and snapshot a page blob in the newly created container.  
+            CloudPageBlob pageBlob = container.GetPageBlobReference(PageBlobName);
+            await pageBlob.CreateAsync(TotalBlobSize);
+            byte[] samplePagedata = new byte[TotalBlobSize];
+            Random random = new Random();
+            random.NextBytes(samplePagedata);
+            await pageBlob.UploadFromByteArrayAsync(samplePagedata, 0, samplePagedata.Length);
+            CloudPageBlob pageBlobSnap = await pageBlob.CreateSnapshotAsync();
+
+            // Create a blob in the backup account
+            CloudPageBlob pageBlobBackup = backupContainer.GetPageBlobReference("copy-of-" + PageBlobName);
+
+            // Copy to the blob in the backup account. Notice that the <isServiceCopy> flag here is 
+            // set to 'false'. This forces data to be first downloaded from the source, then uploaded 
+            // to the destination. If <isServiceCopy> were set to 'true' we could avoid the I/O of 
+            // downloading and then uploading, but could not guarantee  that the copy completed even 
+            // after the local task completed. Since the remainder of this sample depends on the 
+            // successful completion of this operation, we must set <isServiceCopy> to 'false.'
+            await TransferManager.CopyAsync(pageBlobSnap, pageBlobBackup, false);
+
+            // Snapshot the backup copy
+            CloudPageBlob pageBlobBackupSnap = await pageBlob.CreateSnapshotAsync();
+
+            // Change the contents of the original page blob (note: we must convert the byte array to a 
+            // stream in order to write to a non-zero offset in the page blob) and snapshot.
+            byte[] updatePageData = new byte[UpdateWriteSize];
+            random.NextBytes(updatePageData);
+            Stream updatePageDataStream = new MemoryStream(updatePageData);
+            await pageBlob.WritePagesAsync(updatePageDataStream, UpdateWriteOffset, null);
+            await pageBlob.ClearPagesAsync(ClearPagesOffset, ClearPagesSize);
+            CloudPageBlob newPageBlobSnap = await pageBlob.CreateSnapshotAsync();
+
+            // Get the incremental changes and write to the backup.
+            IEnumerable<PageDiffRange> changedPages = 
+                await newPageBlobSnap.GetPageRangesDiffAsync(pageBlobSnap.SnapshotTime.Value);
+            foreach (PageDiffRange pageRange in changedPages)
+            {
+                // If this page range is cleared, remove the old data in the backup.
+                if (pageRange.IsClearedPageRange) await pageBlobBackup.ClearPagesAsync(
+                    pageRange.StartOffset, pageRange.EndOffset - pageRange.StartOffset + 1);
+
+                // If this page range is not cleared, write the new data to the backup.
+                else
+                {
+                    byte[] toWrite = new byte[pageRange.EndOffset - pageRange.StartOffset + 1];
+                    await newPageBlobSnap.DownloadRangeToByteArrayAsync(
+                        toWrite, 0, pageRange.StartOffset, pageRange.EndOffset - pageRange.StartOffset + 1);
+                    Stream toWriteStream = new MemoryStream(toWrite);
+                    await pageBlobBackup.WritePagesAsync(toWriteStream, pageRange.StartOffset, null);
+                }
+            }
+            // Snapshot the backup blob and delete the old snapshot from the primary account 
+            CloudPageBlob pageBlobBackupSnapv2 = await pageBlobBackup.CreateSnapshotAsync();
+            await pageBlobSnap.DeleteAsync();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,36 @@
+---
+services: storage
+platforms: dotnet
+author: michaelhauss
+---
+
+# Backup Azure Virtual Machine Disks with Incremental Snapshots
+
+In this sample, we demonstrate the basics of how to maintain backups of virtual machine disks using snapshots. You can use this methodology when you choose not to use Azure Backup and Recovery Service, and wish to create a custom backup strategy for your virtual machine disks.
+
+If you don't have a Microsoft Azure subscription you can
+get a FREE trial account [here](http://go.microsoft.com/fwlink/?LinkId=330212)
+
+## Running this sample
+
+This sample can be run using either the Azure Storage Emulator that installs as part of the Azure SDK (In Windows only) - or by
+updating the storage connection strings in the app.config file.
+
+To run the sample using the Storage Emulator (Azure SDK):
+
+1. Download and Install the Azure Storage Emulator [here](http://azure.microsoft.com/en-us/downloads/).
+2. Start the Azure Storage Emulator (once only) by pressing the Start button or the Windows key and searching for it by typing "Azure Storage Emulator". Select it from the list of applications to start it.
+3. Set breakpoints and run the project using F10.
+
+To run the sample using the Storage Service
+
+1. Open the app.config file and comment out the connection string for the emulator (UseDevelopmentStorage=True) and uncomment the connection string for the storage service (AccountName=[]...)
+2. Create a two Storage Accounts through the Azure Portal (one primary, one for backup)
+3. Provide your [AccountName] and [AccountKey] in the App.Config file for each of the keys: "StorageConnectionString" and "BackupStorageConnectionString".
+4. Set breakpoints and run the project using F10.
+
+## More information
+- [Protecting Your Blobs Against Application Errors](https://blogs.msdn.microsoft.com/windowsazurestorage/2010/04/29/protecting-your-blobs-against-application-errors/)
+- [Back up Azure Virtual Machines](https://azure.microsoft.com/en-us/documentation/articles/backup-azure-vms/)
+- [Back up Azure Virtual Machine Disks with Incremental Snapshots](https://acom-sandbox.azurewebsites.net/en-us/documentation/articles/storage-incremental-snapshots/)
+- [Storage Emulator](https://azure.microsoft.com/en-us/documentation/articles/storage-use-emulator/)
